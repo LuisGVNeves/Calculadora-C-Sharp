6f85b70043c560898a095bafe23d63f5e75e6c48
Luis Guilherme Vieira Neves <luisguilhermevnevesnogueira@gmail.com>
2023-01-08 15:15:47 -0300

    Add criação do método de subtrair da calculadora

--- Calculadora/Calculadora/Program.cs
+++ Calculadora/Calculadora/Program.cs
@@ -16,6 +16,19 @@
             Console.WriteLine($"O resultado da soma é: {soma}");
         }
 
+        static void Subtracao(double valor = 0, double valor2 = 0)
+        {
+            Console.Write("Primeiro valor: ");
+            valor = double.Parse(Console.ReadLine());
+
+            Console.Write("Segundo valor: ");
+            valor2 = double.Parse(Console.ReadLine());
+
+            double subtracao = valor - valor2;
+
+            Console.WriteLine($"O resultado da subtração é: {subtracao}");
+        }
+
         static void Main(string[] args) 
         {
 
