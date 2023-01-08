3cb0ff222dc2b86bbbba891d94951a0b1bbfabbe
Luis Guilherme Vieira Neves <luisguilhermevnevesnogueira@gmail.com>
2023-01-08 15:17:40 -0300

    Add criação do método de dividir e multiplicar

--- Calculadora/Calculadora/Program.cs
+++ Calculadora/Calculadora/Program.cs
@@ -29,10 +29,38 @@
             Console.WriteLine($"O resultado da subtração é: {subtracao}");
         }
 
+        static void Divisao(double valor = 0, double valor2 = 0)
+        {
+            Console.Write("Primeiro valor: ");
+            valor = double.Parse(Console.ReadLine());
+
+            Console.Write("Segundo valor: ");
+            valor2 = double.Parse(Console.ReadLine());
+
+            double divisao = valor / valor2;
+
+            Console.WriteLine($"O resultado da divisão é: {divisao}");
+        }
+
+        static void Multiplicacao(double valor = 0, double valor2 = 0)
+        {
+            Console.Write("Primeiro valor: ");
+            valor = double.Parse(Console.ReadLine());
+
+            Console.Write("Segundo valor: ");
+            valor2 = double.Parse(Console.ReadLine());
+
+            double multiplicacao = valor * valor2;
+
+            Console.WriteLine($"O resultado da multiplicação é: {multiplicacao}");
+        }
+
         static void Main(string[] args) 
         {
 
             Somar();
+            Subtracao();
+            Divisao();
 
         }
     }
