ae17b3d24281ff236f44d820386cab5a2c17566f
Luis Guilherme Vieira Neves <luisguilhermevnevesnogueira@gmail.com>
2023-01-08 15:14:55 -0300

    Add criação do método de somar da calculadora

--- Calculadora/Calculadora/Program.cs
+++ Calculadora/Calculadora/Program.cs
@@ -1,1 +1,26 @@
-﻿
+﻿namespace Calculadora
+{
+    class Program
+    {
+
+        static void Somar(double valor = 0, double valor2 = 0)
+        {
+            Console.Write("Primeiro valor: ");
+            valor = double.Parse(Console.ReadLine());
+
+            Console.Write("Segundo valor: ");
+            valor2 = double.Parse(Console.ReadLine());
+
+            double soma = valor + valor2;
+
+            Console.WriteLine($"O resultado da soma é: {soma}");
+        }
+
+        static void Main(string[] args) 
+        {
+
+            Somar();
+
+        }
+    }
+}
